1aafdf50c451c8b693b91ac12025ee6ba26dd6dd
Chris Dalke <chrisdalke@gmail.com>
2019-04-26 12:11:55 -0400

    Switched throttle board to node protocol, implemented gps/imu board

--- 2019/IMUBoard/IMUBoard.ino
+++ 2019/IMUBoard/IMUBoard.ino
@@ -3,65 +3,160 @@
 #include <Adafruit_LSM303_U.h>
 #include <Adafruit_L3GD20_U.h>
 #include <Adafruit_9DOF.h>
+#include <Adafruit_GPS.h>
+#include <telemetryNode.h>
 
-
+// Initialize 9-DOF Sensor
 Adafruit_9DOF                 dof   = Adafruit_9DOF();
 Adafruit_LSM303_Accel_Unified accel = Adafruit_LSM303_Accel_Unified(30301);
 Adafruit_LSM303_Mag_Unified   mag   = Adafruit_LSM303_Mag_Unified(30302);
 float seaLevelPressure = SENSORS_PRESSURE_SEALEVELHPA;
 
+// Initialize GPS
+#define GPSSerial Serial1
+Adafruit_GPS GPS(&GPSSerial);
+
+// State variables
 boolean hasAccelerometer;
 boolean hasMag;
+boolean hasGps;
+
+// IMU variables
+const int rollingAverageSize = 16;
+int rollingAverageIndex = 0;
+float imuPitch[rollingAverageSize]; // Pitch axis
+float imuYaw[rollingAverageSize]; // Yaw axis
+float imuRoll[rollingAverageSize]; // Roll axis
+float imuAvgPitch;
+float imuAvgYaw;
+float imuAvgRoll;
+
+// GPS variables
+int gpsFix;
+int gpsFixQuality;
+int gpsNumSatellites;
+float gpsLatitude;
+float gpsLongitude;
+float gpsAltitude;
+int gpsHeading; // int (0-360)
+float gpsSpeedKnots;
+
+
+// Telemetry Protocol Setup
+//GPSIMUNode gpsImuNode(&Serial);
 
 void setup() {
+  pinMode(9, OUTPUT);
+  digitalWrite(9, HIGH);
 
+  Serial.begin(115200);
+
+  initIMU();
+  initGPS();
+  
+  digitalWrite(9, LOW);
 }
 
 void loop() {
-sensors_event_t accel_event;
+  pollGPS();
+  pollIMU();
+  
+  if (gpsFix){
+    digitalWrite(9, (millis() % 100) > 50);
+  } else {
+    digitalWrite(9, (millis() % 2000) < 1000);
+  }
+
+  imuAvgPitch = 0;
+  imuAvgYaw = 0;
+  imuAvgRoll = 0;
+  for (int i = 0; i < rollingAverageSize; i++){
+    imuAvgPitch += imuPitch[i];
+    imuAvgYaw += imuYaw[i];
+    imuAvgRoll += imuRoll[i];
+  }
+  imuAvgPitch /= rollingAverageSize;
+  imuAvgYaw /= rollingAverageSize;
+  imuAvgRoll /= rollingAverageSize;
+
+  /*
+  gpsImuNode.imuPitch = imuAvgPitch;
+  gpsImuNode.imuYaw = imuAvgYaw;
+  gpsImuNode.imuRoll = imuAvgRoll;
+    gpsImuNode.numSatellites = gpsNumSatellites;
+    
+  if (gpsFix){
+    gpsImuNode.latitude = gpsLatitude;
+    gpsImuNode.longitude = gpsLongitude;
+    gpsImuNode.speedKnots = gpsSpeedKnots;
+    gpsImuNode.heading = (int)constrain(map(gpsHeading,0,360,0,255),0,255);
+  } else {
+    gpsImuNode.latitude = 0;
+    gpsImuNode.longitude = 0;
+    gpsImuNode.speedKnots = 0;
+    //gpsImuNode.numSatellites = 0;
+    gpsImuNode.heading = 0;
+  }
+  //gpsImuNode.update();
+  */
+  
+  //Serial.print(String(imuAvgPitch)+" ");
+  //Serial.print(String(imuAvgYaw)+" ");
+  //Serial.println(String(imuAvgRoll));
+  
+  rollingAverageIndex++;
+  if (rollingAverageIndex >= rollingAverageSize){
+    rollingAverageIndex = 0;
+  }
+  delay(10);
+}
+
+void initIMU(){
+  hasAccelerometer = accel.begin();
+  hasMag = mag.begin();
+}
+
+void pollIMU(){
+  sensors_event_t accel_event;
   sensors_event_t mag_event;
   sensors_vec_t   orientation;
 
-  /* Calculate pitch and roll from the raw accelerometer data */
-  accel.getEvent(&accel_event);
-  if (dof.accelGetOrientation(&accel_event, &orientation))
-  {
-    /* 'orientation' should have valid .roll and .pitch fields */
-    Serial.print(F("Roll: "));
-    Serial.print(orientation.roll);
-    Serial.print(F("; "));
-    Serial.print(F("Pitch: "));
-    Serial.print(orientation.pitch);
-    Serial.print(F("; "));
+  if (hasAccelerometer & hasMag){
+    accel.getEvent(&accel_event);
+    mag.getEvent(&mag_event);
+    if (dof.accelGetOrientation(&accel_event, &orientation)){
+      imuRoll[rollingAverageIndex] = orientation.roll;
+      imuPitch[rollingAverageIndex] = orientation.pitch;
+    }
+    if (dof.magTiltCompensation(SENSOR_AXIS_Z, &mag_event, &accel_event)){
+      if (dof.magGetOrientation(SENSOR_AXIS_Z, &mag_event, &orientation)){
+        imuYaw[rollingAverageIndex] = orientation.heading;
+      }
+    }
   }
-  
-  /* Calculate the heading using the magnetometer */
-  mag.getEvent(&mag_event);
-  if (dof.magGetOrientation(SENSOR_AXIS_Z, &mag_event, &orientation))
-  {
-    /* 'orientation' should have valid .heading data now */
-    Serial.print(F("Heading: "));
-    Serial.print(orientation.heading);
-    Serial.print(F("; "));
-  }
-
-  Serial.println(F(""));
-  delay(1000);
 }
 
-void initSensors()
-{
-  if(!accel.begin())
-  {
-    /* There was a problem detecting the LSM303 ... check your connections */
-    Serial.println(F("Ooops, no LSM303 detected ... Check your wiring!"));
-    while(1);
+void initGPS(){
+  GPS.begin(9600);
+  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
+  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);
+  GPS.sendCommand(PGCMD_ANTENNA);
+}
+
+void pollGPS(){
+  char c = GPS.read();
+  if (GPS.newNMEAreceived()){
+    GPS.parse(GPS.lastNMEA());
   }
-  if(!mag.begin())
-  {
-    /* There was a problem detecting the LSM303 ... check your connections */
-    Serial.println("Ooops, no LSM303 detected ... Check your wiring!");
-    while(1);
+  gpsFix = GPS.fix;
+  gpsFixQuality = GPS.fixquality;
+  gpsNumSatellites = GPS.satellites;
+  if (gpsFix){
+    gpsLatitude = GPS.latitude;
+    gpsLongitude = GPS.longitude;
+    gpsAltitude = GPS.altitude;
+    gpsHeading = GPS.angle;
+    gpsSpeedKnots = GPS.speed;
   }
 }
 
--- 2019/ThrottleBoard/ThrottleBoard.ino
+++ 2019/ThrottleBoard/ThrottleBoard.ino
@@ -1,25 +1,21 @@
+#include <telemetryNode.h>
+
 float rawThrottle = 0;
 bool throttleMode = false;
 
+ThrottleNode throttleNode(&Serial);
+
 void setup() {
-  Serial.begin(115200);
   pinMode(5,OUTPUT); //yellow
   pinMode(6,OUTPUT); //green
   pinMode(9,OUTPUT); //red
-  pinMode(10,INPUT_PULLUP); // throttle mode
-  digitalWrite(5,HIGH);
-  delay(100);
-  digitalWrite(6,HIGH);
-  delay(100);
-  digitalWrite(9,HIGH);
-  delay(100);
-  digitalWrite(5,LOW);
-  digitalWrite(6,LOW);
-  digitalWrite(9,LOW);
   
+  pinMode(10,INPUT_PULLUP); // throttle mode
+
 }
 
 void loop() {
+  
   rawThrottle = analogRead(A1) / 1024.0;
   rawThrottle = rawThrottle * rawThrottle;
   throttleMode = !digitalRead(10);
@@ -27,9 +23,8 @@ void loop() {
   digitalWrite(6,throttleMode);
   analogWrite(9,rawThrottle * 255);
 
-  Serial.println("throttleInput:"+String(rawThrottle*100));
-  Serial.println("throttleMode:"+String(throttleMode));
-  
+  throttleNode.throt = (uint16_t)constrain(rawThrottle * 255,0,255);
+  throttleNode.update();
   delay(10);
 }
 
